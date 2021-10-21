013ca59fd4db5a497f590e3c57856dc2e020c99b
Julia Gorelikova <jkireenko137@gmail.com>
2021-10-21 09:08:29 +0300

    добавлен LoginPage

---
+++ src/test/java/PageObject/LoginPage.java
@@ -0,0 +1,48 @@
+package PageObject;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.page;
+
+public class LoginPage {
+    @FindBy(how = How.XPATH, using = "//input[@id='login-form-username']")
+    private SelenideElement UserName;
+
+    @FindBy(how = How.XPATH, using = "//input[@id='login-form-password']")
+    private SelenideElement PassWord;
+
+    @FindBy(how = How.XPATH, using = "//input[@id='login-form-remember-me']")
+    private SelenideElement CheckboxRemember;
+
+    @FindBy(how = How.XPATH, using = "//div[@class='buttons']//input[@id='login']")
+    private SelenideElement buttonLogin;
+
+    @FindBy(how = How.XPATH, using = "//div[@class='aui-page-header-main']/h1")
+    private SelenideElement successfulRegistr;
+
+    public LoginPage clickToValUserName(String login){
+        UserName.click();
+        UserName.sendKeys(login);
+        return page(LoginPage.class);
+    }
+    public LoginPage clickToValPassWord(String password) {
+        PassWord.click();
+        PassWord.sendKeys(password);
+        return page(LoginPage.class);
+    }
+    public LoginPage clickToCheckbox() {
+        CheckboxRemember.click();
+        return page(LoginPage.class);
+    }
+    public MainPage clickButtonLogin() {
+        buttonLogin.click();
+        return page(MainPage.class);
+    }
+    public MainPage should_be_authorized_user(){
+        successfulRegistr.shouldHave(text("System Dashboard"));
+        return page(MainPage.class);
+    }
+}
