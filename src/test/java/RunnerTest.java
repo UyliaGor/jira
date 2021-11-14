576823767069cbf5e92b3fd1ae8a81b7810a557b
Julia Gorelikova <jkireenko137@gmail.com>
2021-11-15 01:12:58 +0300

    отчет для аллюра не из того теста

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,12 +1,28 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = { "hooks", "WebSteps"},
+        plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", "json:target/cucumber,json" },
         tags = "@TEST"
 )
 
 public class RunnerTest {
+    @BeforeClass
+    public static void allureSubThreadParallel() {
+        String listenerName = "AllureSelenide";
+
+        if (!(SelenideLogger.hasListener(listenerName)))
+            SelenideLogger.addListener(listenerName,
+                    new AllureSelenide().
+                            screenshots(true).
+                            savePageSource(false));
+    }
+
 }
--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -2,14 +2,19 @@ package hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
 public class WebHooks {
     @Before
     public void setup(){
     Configuration.startMaximized = true;
     Configuration.timeout = 10000;
     }
+
     @After
     public void driverClose() {WebDriverRunner.closeWebDriver();
     }
