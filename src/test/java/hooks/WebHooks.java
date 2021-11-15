8965019290464969c18eec0d2ea7033300076d12
Julia Gorelikova <jkireenko137@gmail.com>
2021-11-15 10:39:40 +0300

    добавлен allure

--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -2,11 +2,8 @@ package hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 
 public class WebHooks {
     @Before
